cbda765dc5bfe87c25613c071b0cda156acc6fce
Deepak Pathak <imdeepakpathak@gmail.com>
2023-05-02 11:28:55 +0700

    chore: implement list and task creation mutation

--- src/api/resolvers/mutation.js
+++ src/api/resolvers/mutation.js
@@ -1,4 +1,19 @@
-export function createList(parent, args, context) {}
-export function createTask(parent, args, context) {}
+export async function createList(parent, args, context) {
+   return context.prisma.list.create({
+        data:{
+          title: args.title,
+        }
+      })
+}
+export function createTask(parent, args, context) {
+    return context.prisma.task.create({
+        data: {
+          title: args.title,
+          completed: false,
+          list: {connect: {id: args.listID}}
+          
+        }
+      })
+}
 export function updateTask(parent, args, context) {}
 export function moveTask(parent, args, context) {}
