855596cce400c407212bbcd343cdfc0c6d39977a
Deepak Pathak <imdeepakpathak@gmail.com>
2023-05-02 12:01:20 +0700

    chore: apply position to createTask mutation

--- src/api/resolvers/mutation.js
+++ src/api/resolvers/mutation.js
@@ -1,19 +1,28 @@
 export async function createList(parent, args, context) {
-   return context.prisma.list.create({
-        data:{
-          title: args.title,
-        }
-      })
+  return context.prisma.list.create({
+    data: {
+      title: args.title,
+    }
+  })
 }
-export function createTask(parent, args, context) {
-    return context.prisma.task.create({
-        data: {
-          title: args.title,
-          completed: false,
-          list: {connect: {id: args.listID}}
-          
+export async function createTask(parent, args, context) {
+  const listTasksCount = await context.prisma.task.count({
+    where: {
+      listId: args.listID,
+    }
+  })
+  return context.prisma.task.create({
+    data: {
+      title: args.title,
+      completed: false,
+      position : listTasksCount+ 1,
+      list: {
+        connect: {
+          id: args.listID
         }
-      })
+      }
+    }
+  })
 }
 export function updateTask(parent, args, context) {}
 export function moveTask(parent, args, context) {}
