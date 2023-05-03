2ff930173032ed9800cee3cc55687db5f203102f
Deepak Pathak <imdeepakpathak@gmail.com>
2023-05-04 02:44:26 +0700

    test: createList tests

--- src/api/test/mutation.test.js
+++ src/api/test/mutation.test.js
@@ -39,8 +39,36 @@ describe('createList()', () => {
 
     it('should create a new list', async () => {
         const args = { title: "My Todo list" }
+        const newList = {
+            id: 1,
+            title: args.title,
+        }
+        prisma.list.create.mockResolvedValue(newList);
         const ctx = { prisma }
         const result = await createList({}, args, ctx);
         expect(prisma.list.create).toHaveBeenCalled()
+        expect(result).toEqual(newList);
+        expect(prisma.list.create).toHaveBeenCalledWith({
+            data: {
+                title: args.title,
+            }
+        })
+    });
+
+    it('should return GraphQLError when create mutation failed', async () => {
+        const args = { title: "My Todo list" }
+        const mockedErrResponse = new GraphQLError('internal server error: Variable \"$title\" of required type \"String!\" was not provided.', {
+            extensions: {
+                code: 'BAD_REQIEST',
+                http: {
+                    code: 400
+                },
+            },
+        })
+        prisma.list.create.mockResolvedValue(mockedErrResponse);
+        const ctx = { prisma }
+        const result = await createList({}, args, ctx);
+        expect(prisma.list.create).toHaveBeenCalled()
+        expect(result).toEqual(mockedErrResponse);
     });
 });
