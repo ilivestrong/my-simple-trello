b47fcbd810a0519dbb6790565b95235c12213b8e
Deepak Pathak <imdeepakpathak@gmail.com>
2023-05-04 14:52:13 +0700

    test: updateTask mutation completed

--- src/api/test/mutation.test.js
+++ src/api/test/mutation.test.js
@@ -257,102 +257,93 @@ describe('updateTask()', () => {
         const args = {
             title: ""
         }
-        const result = await updateTask({}, args, {
+        const result = await createTask({}, args, {
             prisma: {}
         });
         expect(result).toEqual(new GraphQLError('title is required'));
     });
 
-    // it('should create a new task with correct id value', async () => {
-    //     const ctx = {
-    //         prisma
-    //     }
-    //     const todoList = {
-    //         id: 1,
-    //         title: "My todo list",
-    //     }
-    //     const taskArgs = {
-    //         title: 'Grocery shopping',
-    //         listID: todoList.id,
-    //     }
-    //     const existingTasksInList = 1
-    //     prisma.task.count.mockResolvedValue(existingTasksInList);
+    it('should update a task with correct input', async () => {
+        const ctx = {
+            prisma
+        }
+        const taskArgs = {
+            title: 'Grocery shopping',
+            completed: true,
+            taskID: 1,
+        }
 
-    //     const result = await createTask({}, taskArgs, ctx);
+        const result = await updateTask({}, taskArgs, ctx);
 
-    //     expect(prisma.task.create).toHaveBeenCalled()
-    //     expect(prisma.task.create).toHaveBeenCalledWith({
-    //         data: {
-    //             title: taskArgs.title,
-    //             completed: false,
-    //             position: existingTasksInList + 1,
-    //             list: {
-    //                 connect: {
-    //                     id: todoList.id,
-    //                 }
-    //             }
-    //         }
-    //     })
-    // });
+        expect(prisma.task.update).toHaveBeenCalled()
+        expect(prisma.task.update).toHaveBeenCalledWith({
+            data: {
+                title: taskArgs.title,
+                completed: taskArgs.completed,
+            },
+            where: {
+                id: taskArgs.taskID,
+            }
+        })
+    });
 
-    // it('should return GraphQLError when listID not found', async () => {
-    //     const ctx = {
-    //         prisma
-    //     }
-    //     const taskArgs = {
-    //         title: 'Grocery shopping',
-    //         listID: -1, // invalid listID provided
-    //     }
-    //     const expected = new GraphQLError(`listID not found in db: ${taskArgs.listID}`, {
-    //         extensions: {
-    //             code: 'NOT_FOUND',
-    //             http: {
-    //                 status: 404,
-    //             },
-    //         },
-    //     });
+    it('should return GraphQLError when taskID not found', async () => {
+        const ctx = {
+            prisma
+        }
+        const taskArgs = {
+            title: 'Grocery shopping',
+            completed: true,
+            taskID: -1, // invalid taskID
+        }
+        const expected = new GraphQLError(`taskID not found in db: ${taskArgs.taskID}`, {
+            extensions: {
+                code: 'NOT_FOUND',
+                http: {
+                    status: 404,
+                },
+            },
+        });
+        prisma.task.update.mockImplementation(() => {
+            const errorNotFound = new Error('not found');
+            errorNotFound.code = ErrCodeRequiredFieldNotFound
+            throw errorNotFound
+        });
 
-    //     prisma.task.count.mockImplementation(() => {
-    //         const errorNotFound = new Error('not found');
-    //         errorNotFound.code = ErrCodeRequiredFieldNotFound
-    //         throw errorNotFound
-    //     });
+        const result = await updateTask({}, taskArgs, ctx);
 
-    //     const result = await createTask({}, taskArgs, ctx);
+        expect(result).toEqual(expected);
+    });
 
-    //     expect(prisma.task.create.mock.calls).toHaveLength(0);
-    //     expect(result).toEqual(expected);
-    // });
+    it('should return GraphQLError when update() call fails with internal error', async () => {
+        const ctx = {
+            prisma
+        }
+        const taskArgs = {
+            title: 'Grocery shopping',
+            completed: true,
+            taskID: 1,
+        }
+        const expected = new GraphQLError(`internal server error on: ${['some field'].join(",")}`, {
+            extensions: {
+                code: 'INTERNAL_ERROR',
+                http: {
+                    status: 500,
+                },
+            },
+        });
 
-    // it('should return GraphQLError when any internal server error', async () => {
-    //     const ctx = {
-    //         prisma
-    //     }
-    //     const taskArgs = {
-    //         title: 'Grocery shopping',
-    //         listID: 1, // invalid listID provided
-    //     }
-    //     const expected = new GraphQLError(`internal server error on: ${['some field'].join(",")}`, {
-    //         extensions: {
-    //             code: 'INTERNAL_ERROR',
-    //             http: {
-    //                 status: 500,
-    //             },
-    //         },
-    //     });
+        prisma.task.update.mockImplementation(() => {
+            const internalError = new Error('internal error');
+            internalError.code = "P2021"
+            internalError.meta = {
+                target: ["some field"]
+            }
+            throw internalError
+        });
 
-    //     prisma.task.count.mockImplementation(() => {
-    //         const internalError = new Error('internal error');
-    //         internalError.code = "P2021"
-    //         internalError.meta = {
-    //             target: ["some field"]
-    //         }
-    //         throw internalError
-    //     });
+        const result = await updateTask({}, taskArgs, ctx);
 
-    //     const result = await createTask({}, taskArgs, ctx);
-
-    //     expect(prisma.task.create.mock.calls).toHaveLength(0);
-    //     expect(result).toEqual(expected);
-    // });
+        expect(result).toEqual(expected);
+    });
 });
